666f3e4162d656fe2702576da4ada23f3634fa5e
Саша Евдокимов <thefreeskystyle@gmail.com>
2023-05-05 09:30:52 +0300

    Добавьте файлы проекта.

---
+++ ETCalculator/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ETCalculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+       
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            // здесь находятся все кнопки калькулятора
+            var text = ((Button)sender).Text;
+            txtInput.Text += text; 
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtInput_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
